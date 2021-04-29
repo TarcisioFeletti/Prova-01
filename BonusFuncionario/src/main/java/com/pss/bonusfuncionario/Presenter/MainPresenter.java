631841516b309276c75eae8f6d5b15145b430f33
Tarcísio feletti <tarcisio.feletti@hotmail.com>
2021-04-28 22:09:52 -0300

    Criação de algumas Views

--- BonusFuncionario/src/main/java/com/pss/bonusfuncionario/Presenter/MainPresenter.java
+++ BonusFuncionario/src/main/java/com/pss/bonusfuncionario/Presenter/MainPresenter.java
@@ -5,7 +5,13 @@
  */
 package com.pss.bonusfuncionario.Presenter;
 
-import com.pss.bonusfuncionario.View.TelaPrincipal;
+import com.pss.bonusfuncionario.View.BuscarFuncionario;
+import com.pss.bonusfuncionario.View.BuscarFuncionarioView;
+import com.pss.bonusfuncionario.View.CalcularSalarioView;
+import com.pss.bonusfuncionario.View.ManterFuncionarioView;
+import com.pss.bonusfuncionario.View.TelaPrincipalView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 /**
@@ -14,6 +20,26 @@ import com.pss.bonusfuncionario.View.TelaPrincipal;
  */
 public class MainPresenter {
     public static void main(String[] args){
-        new TelaPrincipal();
+        TelaPrincipalView tela = new TelaPrincipalView();
+        tela.getOpcaoManterFuncionario().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                tela.getDesktop().add(new ManterFuncionarioView());
+            }
+        });
+        
+        tela.getOpcaoBuscarFuncionario().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                tela.getDesktop().add(new BuscarFuncionario());
+            }
+        });
+        
+        tela.getOpcaoCalcularSalario().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                tela.getDesktop().add(new CalcularSalarioView());
+            }
+        });
     }
 }
